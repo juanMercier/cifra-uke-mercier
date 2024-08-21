e2feefe0c2ffd522468c4e96fe55c6675fef9a7c
MrEltripZ <workingeltripz@gmail.com>
2024-08-21 22:37:16 +0100

    Added pdf to main content

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,54 +1,52 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
+import '../styles.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-
-console.log({version:pdfjs.version})
 const MainContent = ({ selectedLyrics, scrollSpeed, onScrollSpeedChange, isOpen }) => {
   const lyricsDisplayRef = useRef(null);
-  const [numPages, setNumPages] = useState(null);
 
   useEffect(() => {
     let scrollInterval;
-    if (selectedLyrics && scrollSpeed > 0) {
-      scrollInterval = setInterval(() => {
+
+    if (selectedLyrics && scrollSpeed > 0 && lyricsDisplayRef.current) {
+      // Scroll function
+      const scroll = () => {
         if (lyricsDisplayRef.current) {
-          lyricsDisplayRef.current.scrollBy(0, 1);
+          lyricsDisplayRef.current.scrollBy(0, scrollSpeed);
         }
-      }, 100 / scrollSpeed);
+      };
+
+      scrollInterval = setInterval(scroll, 100);
     }
 
     return () => clearInterval(scrollInterval);
   }, [scrollSpeed, selectedLyrics]);
 
-  const onDocumentLoadSuccess = ({ numPages }) => {
-    setNumPages(numPages);
-  };
-
   return (
     <div className={`main-content ${isOpen ? '' : 'collapsed'}`}>
       <div ref={lyricsDisplayRef} className="lyrics-display">
         {selectedLyrics ? (
-          <Document
-            file={selectedLyrics.url}
-            onLoadSuccess={onDocumentLoadSuccess}
+          <object
+            data={selectedLyrics.url}
+            type="application/pdf"
+            width="100%"
+            height="100%"
           >
-            {Array.from(new Array(numPages), (el, index) => (
-              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-            ))}
-          </Document>
+            <iframe
+              src={selectedLyrics.url}
+              width="100%"
+              height="100%"
+              style={{ border: 'none' }}
+            >
+              <p>
+                Your browser does not support PDFs.
+                <a href={selectedLyrics.url}>Download the PDF</a>
+              </p>
+            </iframe>
+          </object>
         ) : (
           <pre>Select a song from the sidebar.</pre>
         )}
       </div>
-      <input
-        type="range"
-        min="1"
-        max="10"
-        value={scrollSpeed}
-        onChange={(e) => onScrollSpeedChange(Number(e.target.value))}
-      />
     </div>
   );
 };
